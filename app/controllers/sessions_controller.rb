3e67d8145ea4e6fc1b9bfc8cc13dd359ab334a09
Adam Urbina <adamurbina@gmail.com>
2018-11-26 13:25:32 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if !user.nil?
       session[:user_id] = user.id
     else
+      user = User.new(auth_hash)
       user = User.new(name: auth_hash[:info][:name], email: auth_hash[:info][:email], uid: auth_hash[:uid])
       user.save
       session[:user_id] = user.id
