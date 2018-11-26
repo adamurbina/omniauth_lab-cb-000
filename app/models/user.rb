56ed9427859f46a74fd4ba20a3f03d1078eacd61
Adam Urbina <adamurbina@gmail.com>
2018-11-26 13:46:59 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_by_auth(auth_hash)
     self.where(uid: auth_hash[:uid]).first_or_create do |user|
-
+      user.provider = auth_hash[:provider]
+      user.email = auth_hash[:info][:email]
     end
   end
 
