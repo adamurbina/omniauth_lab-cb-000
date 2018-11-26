4c8d84f2e12eb29c0766a858e8b2fc41dc8adcf7
Adam Urbina <adamurbina@gmail.com>
2018-11-26 13:53:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,9 @@ class SessionsController < ApplicationController
 
     auth_hash = request.env['omniauth.auth']
 
-    # user = User.find_by(uid: auth_hash['uid'])
     user = User.find_or_create_by_auth(auth_hash)
     session[:user_id] = user.id
 
-    # if !user.nil?
-    #   session[:user_id] = user.id
-    # else
-    #   user = User.new(name: auth_hash[:info][:name], email: auth_hash[:info][:email], uid: auth_hash[:uid])
-    #   user.save
-    #   session[:user_id] = user.id
-    # end
-
   end
-
-  private
-
-  # def auth_hash
-  #   auth_hash = request.env['omniauth_auth']
-  # end
 
 end
